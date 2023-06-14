d17f9b0d58cb21fa71b51b96b924c8996aa8f74f
Killswitch0 <maximpanin1996@gmail.com>
2023-06-14 22:14:49 +0300

    Refactor some code with Altivi

--- app/helpers/habits_helper.rb
+++ app/helpers/habits_helper.rb
@@ -1,11 +1,7 @@
 module HabitsHelper
   def habits_completed(goal)
-    completed = CompletionDate
-                  .where('habit_id IN (?) AND created_at >= ?',
-                         goal.habits.select(:id), Date.today.beginning_of_day).count
-
+    completed = goal.habits.completed(goal).count
     all = goal.habits.count
-    habits = completed == 0 ? 0 : completed
-    "#{habits}/#{all}"
+    "#{completed}/#{all}"
   end
 end
