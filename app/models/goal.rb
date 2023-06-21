347472eca05df49545bc145f7d5e457e40557b5a
MaxPayne <maximpanin1996@gmail.com>
2023-06-21 20:50:23 +0300

    Add goal mark in calendar for habits

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,13 @@
 class Goal < ApplicationRecord
+  AVAILABLE_COLORS = [['Purple', 'rgb(140,8,246)'],
+                      ['Blue', 'rgb(17,85,253)'], 
+                      ['Red', 'rgb(255,18,12)'], 
+                      ['Pink', 'rgb(255,125,253)'],
+                      ['Cyan', 'rgb(1,240,253)'],
+                      ['Yellow', 'rgb(255,255,33)'],
+                      ['Gold', 'rgb(252,206,24)'],
+                      ['Midnight blue', 'rgb(14,31,82)']]
+
   belongs_to :user
   belongs_to :category
 
@@ -14,6 +23,7 @@ class Goal < ApplicationRecord
             length: { minimum: 7, maximum: 200 }
 
   validates :category_id, presence: true
+  validates :color, presence: true
 
   def complete?
     self.complete == true
