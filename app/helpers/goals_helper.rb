2879a83d9470c76772ba47db2e26715e3514ec6a
Killswitch0 <maximpanin1996@gmail.com>
2023-07-06 13:35:05 +0300

    Move colors constant to goals_helper

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -1,6 +1,15 @@
 module GoalsHelper
   include TasksHelper
 
+  AVAILABLE_COLORS = [['Purple', 'rgb(140,8,246)'],
+                      ['Blue', 'rgb(17,85,253)'],
+                      ['Red', 'rgb(255,18,12)'],
+                      ['Pink', 'rgb(255,125,253)'],
+                      ['Cyan', 'rgb(1,240,253)'],
+                      ['Yellow', 'rgb(255,255,33)'],
+                      ['Gold', 'rgb(252,206,24)'],
+                      ['Midnight blue', 'rgb(14,31,82)']]
+
   def complete?
     complete == true
   end
@@ -10,4 +19,6 @@ module GoalsHelper
     goals = goals.count
     "#{comp}/#{goals}"
   end
+
+
 end
