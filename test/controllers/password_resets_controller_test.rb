f8359229792ebb60411b34d582abb8c458ca89e3
Max <89165782+Killswitch0@users.noreply.github.com>
2023-07-07 13:37:23 +0300

    Delete test directory

--- test/controllers/password_resets_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get password_resets_new_url
-    assert_response :success
-  end
-end
--- test/models/completion_date_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CompletionDateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
