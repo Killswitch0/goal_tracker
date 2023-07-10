9fffe6517124bd91b31f8c57639e316fbce862ad
Killswitch0 <maximpanin1996@gmail.com>
2023-07-10 13:10:58 +0300

    Add notify_almost_streak notification to Task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,7 +5,7 @@ class Task < ApplicationRecord
   validates :name, presence: true
 
   after_create_commit :notify_create
-  after_initialize
+  after_update_commit :notify_almost_streak, if: :almost_streak?
 
   before_destroy :clean_up_notifications
 
@@ -16,8 +16,20 @@ class Task < ApplicationRecord
 
   private
 
+  def almost_streak?
+    all_tasks = goal.tasks.count
+    completed = goal.tasks.where(complete: true).size
+    (all_tasks - completed).between?(1, 4) &&
+      all_tasks != 0 &&
+      self.complete?
+  end
+
   def notify_create
     TaskNotification.with(task: self, goal: goal).deliver(goal.user)
+  end
+
+  def notify_almost_streak
+    TaskAlmostNotification.with(task: self, goal: goal).deliver(goal.user)
   end
 
   def cleanup_notifications
---
+++ app/notifications/tasks/task_almost_notification.rb
@@ -0,0 +1,28 @@
+# To deliver this notification:
+#
+# TaskAlmostNotification.with(post: @post).deliver_later(current_user)
+# TaskAlmostNotification.with(post: @post).deliver(current_user)
+
+class TaskAlmostNotification < Noticed::Base
+  # Add your delivery methods
+  #
+  deliver_by :database
+  # deliver_by :email, mailer: "UserMailer"
+  # deliver_by :slack
+  # deliver_by :custom, class: "MyDeliveryMethod"
+
+  # Add required params
+  #
+  # param :post
+
+  # Define helper methods to make rendering easier.
+
+  def message
+    @goal = Goal.find(params[:task][:goal_id])
+    "You almost made tasks streak in #{@goal.name.truncate(10)}"
+  end
+
+  def url
+    goal_path(Goal.find(params[:task][:goal_id]), mark_as_read: 'true')
+  end
+end
