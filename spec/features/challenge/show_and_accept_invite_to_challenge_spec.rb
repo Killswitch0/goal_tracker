a13cddbb271173e07721de39d1af5ec9b556f5b0
Killswitch0 <maximpanin1996@gmail.com>
2023-09-27 22:45:36 +0300

    Add show_and_accept_invite_to_challenge_spec

--- spec/features/challenge/show_and_accept_invite_to_challenge_spec.rb
+++ spec/features/challenge/show_and_accept_invite_to_challenge_spec.rb
@@ -1,14 +1,10 @@
 require 'rails_helper'
 
 RSpec.feature "ShowAndAcceptInviteToChallenges" do
+  given(:user) { create :user }
+  given(:user2) { create :user }
 
-  given!(:user) { create :user }
-  given!(:user2) { create :user }
-
-  given!(:goal1) { create :goal, user: user }
-  given!(:goal2) { create :goal, user: user2, name: 'Buy home', color: 'red' }
-
-  given!(:challenge) { create :challenge, user: user }
+  given(:challenge) { create :challenge, user: user }
   given!(:challenge_user2) { create :challenge_user, challenge: challenge, user: user2, confirm: false }
 
   feature 'Show invite', %q{
@@ -16,14 +12,25 @@ RSpec.feature "ShowAndAcceptInviteToChallenges" do
     we need to be able to see him
   } do
 
-    scenario 'Authenticated user try to see invite to Challenge' do
+    before do
       log_in(user2)
 
       visit challenges_path
+    end
 
+    scenario 'Authenticated user try to see invite to Challenge' do
       find('[@id="challenges-index"]/div[1]/div/div[2]/button').click
-
       expect(page).to have_css('[@id="offcanvasExample"]/div[2]/div/table/tbody/tr', text: challenge.user.name)
+    end
+
+    scenario 'Authenticated user try to accept invite to Challenge' do
+      find('[@id="offcanvasExample"]/div[2]/div/table/tbody/tr[2]/td/form/button', text: 'Accept').click
+      expect(page).to have_content I18n.t('challenges.confirm_invitation.success', challenge_name: challenge.name)
+    end
+
+    scenario 'Authenticated user try to decline invite to Challenge' do
+      find('[@id="offcanvasExample"]/div[2]/div/table/tbody/tr[2]/td/form/button', text: 'Decline').click
+      expect(page).to have_content I18n.t('challenges.decline_invitation.success', challenge_name: challenge.name)
     end
   end
 end
