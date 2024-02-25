eb8e4b5ab5376b61115bd9edb79dbb691075c22a
Max <maximpanin1996@gmail.com>
2024-02-25 21:12:06 +0200

    Add week and month period scenarios to chart_task_spec

--- spec/features/charts/chart_task_spec.rb
+++ spec/features/charts/chart_task_spec.rb
@@ -1,22 +1,32 @@
 require 'rails_helper'
 
-RSpec.feature "ChartTasks", type: :feature do
+RSpec.feature 'ChartTasks', type: :feature do
   let(:user) { create(:user) }
 
   before do
     log_in(user)
 
     visit task_chart_path(period: 'day')
+    expect(page).to have_content 'TASKS CHART'
+    expect(find('.chart-for-dropdown')).to have_content('Tasks')
   end
 
   feature 'User use Task chart', '
     In order to see my progress on chart
     i want to use all chart functional
   ' do
-      scenario 'User visit to Task chart' do
-        expect(page).to have_content 'TASKS CHART'
-        expect(find('.chart-for-dropdown')).to have_content('Tasks')
-        expect(page).to have_css('a.day-link', class: 'active')
-      end
+    scenario 'User visit to Task chart' do
+      expect(page).to have_css('a.day-link', class: 'active')
     end
+
+    scenario 'User switch chart period to Week' do
+      click_link 'Week'
+      expect(page).to have_css('a.week-link', class: 'active')
+    end
+
+    scenario 'User switch chart period to Month' do
+      click_link 'Month'
+      expect(page).to have_css('a.month-link', class: 'active')
+    end
+  end
 end
